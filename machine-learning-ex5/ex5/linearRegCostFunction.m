daec97fa3ec256d439697aebd80e73a85fa2f53c
Sara <saramh11@gmail.com>
2016-07-06 23:46:29 -0500

    ex5 is finished. linearRegCostFunction implements the Cost and gradient for linear regression learningCurve maps the curves to evaluate the algorithm validationCurve shows the curves as a function of lambda to be able to choose it polyFeatures computes the polinomialfeatures

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -20,8 +20,11 @@ grad = zeros(size(theta));
 %
 
 
+J = 1/2/m*sum((X*theta-y).^2)+lambda/2/m*sum(theta(2:end).^2);
 
-
+thetag = theta;
+thetag(1)=0;
+grad = 1/m*((X*theta-y)'*X)'+lambda/m*thetag;
 
 
 
